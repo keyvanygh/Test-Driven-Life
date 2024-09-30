b39d90098f435ef3225ec72bab11478641943af2
Keyvan <Keyvan.ygh@gmail.com>
2024-09-30 04:29:58 -0400

    create  returns Task On Client Success With Correct Data

---
+++ Task Creator/Task Creator/Remote/Model/RemoteTask.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Task Creator
+//
+//  Created by Keyvan Yaghoubian on 9/30/24.
+//
+
+import Foundation
+
+struct RemoteTask: Codable {
+    let title: String
+    
+    var entity: TaskEntity {
+        return TaskEntity(title: title)
+    }
+}
---
+++ Task Creator/Task Creator/Remote/RemoteTaskCreator.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Task Creator
+//
+//  Created by Keyvan Yaghoubian on 9/30/24.
+//
+
+import Foundation
