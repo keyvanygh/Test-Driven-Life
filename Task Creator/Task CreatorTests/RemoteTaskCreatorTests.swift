97a620bfb119d5efb1319c2f3c131f2ff1ba3efd
Keyvan <Keyvan.ygh@gmail.com>
2024-09-30 03:39:36 -0400

    create throw On Not 200 HttpResponse

--- Task Creator/Task CreatorTests/RemoteTaskCreatorTests.swift
+++ Task Creator/Task CreatorTests/RemoteTaskCreatorTests.swift
@@ -15,10 +15,13 @@ public protocol TaskCreator {
 }
 
 public final class RemoteTaskCreator: TaskCreator {
-
+    
     let client: HTTPClient
     let url: URL
     
+    enum Error: Swift.Error {
+        case clientNot200Reponse
+    }
     
     init(client: HTTPClient, url: URL) {
         self.client = client
@@ -57,10 +60,57 @@ final class RemoteTaskCreatorTests: XCTestCase {
         XCTAssertEqual(client.requests, [expectedURLRequest])
     }
     
+    func test_create_throwOnNot200HttpResponse() async {
+        let not200StatusCodes = [300, 401, 404, 500, 501, 503, 201, 202]
+        
+        for statusCode in not200StatusCodes {
+            let (sut, client) = makeSUT()
+            
+            await expect(
+                {
+                    try await sut.create()
+                },
+                toThrow: RemoteTaskCreator.Error.clientNot200Reponse,
+                when: {
+                    client.returns(
+                        with: (Data(),
+                               HTTPURLResponse(statusCode: statusCode))
+                    )
+                }
+            )
+        }
+    }
+    
     private func makeSUT(url: URL = .dummy) -> (RemoteTaskCreator, HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteTaskCreator(client: client, url: url)
         return (sut, client)
+    }
+    
+    private func expect<Error: Equatable>(
+        _ action: @escaping () async throws -> (),
+        toThrow expectedError: Error,
+        when: @escaping () -> (),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) async {
+        
+        let task = Task {
+            do {
+                _ = try await action()
+            } catch {
+                let receivedError = error as? Error
+                XCTAssertEqual(receivedError, expectedError)
+            }
+        }
+        
+        await aFewMomentsLater()
+        
+        let when = Task {
+            when()
+        }
+        
+        _ = await (task.value, when.value)
     }
     
     private func aFewMomentsLater() async {
