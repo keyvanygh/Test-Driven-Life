dd689d580f1112069f9c87a37a30abedab31aa77
Keyvan <Keyvan.ygh@gmail.com>
2024-09-30 02:44:32 -0400

    added gitignore

--- Task Creator/Task CreatorTests/RemoteTaskCreatorTests.swift
+++ Task Creator/Task CreatorTests/RemoteTaskCreatorTests.swift
@@ -1,8 +1,42 @@
 //
-//  RemoteTaskCreator.swift
-//  Task CreatorTests
+//  RemoteTaskCreatorTests.swift
 //
 //  Created by Keyvan Yaghoubian on 9/30/24.
 //
 
 import Foundation
+import XCTest
+import HttpClient
+import Task_Creator
+
+public protocol TaskCreator {
+    func create() async throws
+}
+
+public final class RemoteTaskCreator {
+
+    let client: HTTPClient
+    let url: URL
+    
+    
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+}
+
+final class RemoteTaskCreatorTests: XCTestCase {
+    
+    func test_init_doseNotCreateTask() {
+        let (sut, client) = makeSUT()
+        
+        XCTAssertEqual(client.requestedURLs, [])
+    }
+    
+    private func makeSUT(url: URL = .dummy) -> (RemoteTaskCreator, HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteTaskCreator(client: client, url: url)
+        return (sut, client)
+    }
+}
